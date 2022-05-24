5b29e806f5f9d2956e425347569e291ec2b50c74
shahzaib <shahzaibnadeem702@gmail.com>
2022-05-24 12:52:13 +0500

    Initial commit

---
+++ database/factories/TimetableFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Timetable;
+use Faker\Generator as Faker;
+
+$factory->define(Timetable::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
